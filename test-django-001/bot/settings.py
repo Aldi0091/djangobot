64aafa69291eb8f4b36a5c946373792f3a74f605
akhanovkz <akhanovkz@gmail.com>
2023-01-17 20:16:16 +0600

    bot composed

--- test-django-001/bot/settings.py
+++ test-django-001/bot/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'test_base',
         'USER': 'aldo',
-        'PASSWORD': '12346578',
+        'PASSWORD': '12345678',
         'HOST': 'postgres',
         'PORT': '5432',
     }
